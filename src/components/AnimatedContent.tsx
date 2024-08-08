be1a18ab2a7ff7f41dd607427ee5a9409d08d0dc
Alexandru Rachita <alexandrurachita98@gmail.com>
2024-08-08 16:04:51 +0300

    fix responsiveness

--- src/components/AnimatedContent.tsx
+++ src/components/AnimatedContent.tsx
@@ -3,16 +3,22 @@ import { useState, useEffect } from "react";
 export default function AnimatedContent() {
   const [step, setStep] = useState(0);
   const messages = [
-    { text: "Welcome to My Portfolio!", className: "text-5xl mt-6 mb-4" },
-    { text: "I'm Alex, nice to meet you.", className: "text-bold text-4xl mt-6 mb-4" },
+    {
+      text: "Welcome to My Portfolio!",
+      className: "text-3xl md:text-5xl mt-6 mb-4",
+    },
+    {
+      text: "I'm Alex, nice to meet you.",
+      className: "text-bold text-2xl md:text-4xl mt-6 mb-4",
+    },
     {
       text: "I'm a Software Developer from Romania - learn more about me ",
       link: { url: "/about", text: "here" },
-      className: "text-3xl mt-6 mb-4",
+      className: "text-xl md:text-3xl mt-6 mb-4",
     },
     {
       text: "On this page you can see my projects, however fresh or old they might be. You can also see my thoughts if I'm ever in the mood for blogging.",
-      className: "text-3xl mt-6 mb-4",
+      className: "text-xl md:text-3xl mt-6 mb-4",
     },
   ];
 
@@ -31,7 +37,7 @@ export default function AnimatedContent() {
   }, []);
 
   return (
-    <div className="relative max-w-6xl mx-auto px-5 h-full pt-8 md:py-24">
+    <div className="relative max-w-6xl mx-auto px-4 md:px-8 h-full pt-8 md:py-24">
       {messages.slice(0, step + 1).map((message, index) => (
         <p key={index} className={`mb-4 slide-in ${message.className}`}>
           {message.text}
@@ -39,7 +45,8 @@ export default function AnimatedContent() {
             <>
               <a
                 href={message.link.url}
-                className="text-black font-bold no-underline">
+                className="text-black font-bold no-underline"
+              >
                 {message.link.text}
               </a>
               <span>.</span>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,27 @@
-import Image from 'next/image';
-import githubLogo from '../../public/github-mark.png'
-import githubWhiteLogo from '../../public/github-mark-white.png'
-
 export default function Footer() {
-    return (
-      <footer className="flex justify-between items-center h-20 border-t-2 mt-8">
-        <div className="text-lg text-black ml-24 font-light">&copy; 2024 Alexandru Răchită</div>
-        <div className="flex space-x-4 mr-12">
-        <a href="https://github.com/alexrac1607" target="_blank" rel="noopener noreferrer" className="text-black border-2 border-black p-1 rounded hover:bg-black hover:text-white transition">
-          <Image src={githubLogo} alt="GitHub" className="h-8 w-8" />
+  return (
+    <footer className="flex flex-col md:flex-row justify-between items-center h-auto md:h-20 border-t-2 mt-8 px-4 md:px-8 py-4 md:py-0">
+      <div className="text-base md:text-lg text-black font-light mb-4 md:mb-0">
+        &copy; 2024 Alexandru Răchită
+      </div>
+      <div className="flex space-x-4">
+        <a
+          href="https://github.com/alexrac1607"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black border-2 border-black p-1 rounded hover:bg-black hover:text-white transition github-link logo-container"
+        >
+          <div className="image-hover" />
         </a>
-          <a href="https://linkedin.com/in/your-linkedin-username" target="_blank" rel="noopener noreferrer" className="text-black border-2 border-black px-3 py-1 rounded hover:bg-black hover:text-white transition">
-            LinkedIn
-          </a>
-        </div>
-      </footer>
-    );
-  }
-  
+        <a
+          href="https://linkedin.com/in/your-linkedin-username"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black border-2 border-black px-3 py-1 rounded hover:bg-black hover:text-white transition"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </footer>
+  );
+}
