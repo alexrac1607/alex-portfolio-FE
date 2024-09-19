cde1b4740d699fd9f04ba9e5f1564814098100e6
Alexandru Rachita <alexandrurachita98@gmail.com>
2024-09-19 19:14:01 +0300

    abc

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     distDir: 'out',
     basePath: '/alex-portfolio-FE',
-    assetPrefix: 'alex-portfolio-FE/'
+    assetPrefix: 'alex-portfolio-FE/',
+    output: 'export'
 };
 
 module.exports = nextConfig
