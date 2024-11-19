dc491096e6776dbf000cb8464fb0b0e8651712f3
Alexandru Rachita <alexandrurachita98@gmail.com>
2024-11-19 17:36:41 +0200

    try no projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <>
       <AnimatedContent />
-      <ProjectTileContainer projects={projects} />
+      {/* <ProjectTileContainer projects={projects} /> */}
       <BlogTileContainer posts={posts} />
     </>
   );
