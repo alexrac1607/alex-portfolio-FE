85a3eb7f71575f337c93c88ca026380b16fcc7fb
Alexandru Rachita <alexandrurachita98@gmail.com>
2024-11-19 17:15:04 +0200

    add public runtime config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
     distDir: 'out',
     basePath: '/alex-portfolio-FE',
     assetPrefix: '/alex-portfolio-FE/',
-    output: 'export'
+    output: 'export',
+    publicRuntimeConfig: {
+        basePath: '/alex-portfolio-FE', 
+    },
 };
 
 module.exports = nextConfig
